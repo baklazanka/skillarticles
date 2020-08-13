33f5c0e39102e72fdc235cafc14290b6a2c066ac
baklazanka <alina-baklazanka@yandex.ru>
2020-08-14 00:30:47 +0200

    hometask_3: add behaviors.

---
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/custom/behaviors/SubmenuBehavior.kt
@@ -0,0 +1,8 @@
+package ru.skillbranch.skillarticles.ui.custom.behaviors
+
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import ru.skillbranch.skillarticles.ui.custom.ArticleSubmenu
+
+class SubmenuBehavior : CoordinatorLayout.Behavior<ArticleSubmenu>() {
+
+}
