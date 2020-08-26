d34813bc69200594ae8dfe7f898f0c587d607ae4
baklazanka <alina-baklazanka@yandex.ru>
2020-08-26 21:04:38 +0200

    hometask_4: PrefDelegate

---
+++ app/src/main/java/ru/skillbranch/skillarticles/data/delegates/PrefDelegate.kt
@@ -0,0 +1,39 @@
+package ru.skillbranch.skillarticles.data.delegates
+
+import ru.skillbranch.skillarticles.data.local.PrefManager
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
+
+@Suppress("UNCHECKED_CAST")
+class PrefDelegate<T>(private val defaultValue: T) : ReadWriteProperty<PrefManager, T?> {
+
+    override fun getValue(thisRef: PrefManager, property: KProperty<*>): T? {
+        with(thisRef.preferences) {
+            return when (defaultValue) {
+                is Boolean -> getBoolean(property.name, defaultValue) as? T
+                is String -> getString(property.name, defaultValue) as? T
+                is Float -> getFloat(property.name, defaultValue) as? T
+                is Int -> getInt(property.name, defaultValue) as? T
+                is Long -> getLong(property.name, defaultValue) as? T
+                else -> throw NotFoundRealizationException(defaultValue)
+            }
+        }
+    }
+
+    override fun setValue(thisRef: PrefManager, property: KProperty<*>, value: T?) {
+        with(thisRef.preferences.edit()) {
+            when (value) {
+                is Boolean -> putBoolean(property.name, value)
+                is String -> putString(property.name, value)
+                is Float -> putFloat(property.name, value)
+                is Int -> putInt(property.name, value)
+                is Long -> putLong(property.name, value)
+                else -> throw NotFoundRealizationException(defaultValue)
+            }
+            apply()
+        }
+    }
+
+    class NotFoundRealizationException(defaultValue: Any?) : Exception("Not found realization for $defaultValue")
+
+}
